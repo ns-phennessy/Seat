074af83e2566435a95955b1c548a90bcca5f67db
Sammons <bsammnz@gmail.com>
2015-02-19 13:32:36 -0800

    ben: we need to use CSRF correctly, and PAT always POST forms plox

--- Seat/views.py
+++ Seat/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 
 def login(request):
-	return render(request, 'login.html')
+	if (request.method == 'GET'):
+		return render(request, 'login.html')
+	if (request.method == 'POST'):
+		print request.POST
+		return render(request, 'login.html')
