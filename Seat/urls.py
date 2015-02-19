2a82197032d795f9d12db50cd5ffdc5070fcabd2
Sammons <bsammnz@gmail.com>
2015-02-19 12:49:12 -0800

    ben: the server has a login endpoint (next step is to make it serve pat's template)

--- Seat/urls.py
+++ Seat/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from Seat import views
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login',views.login)
 )
---
+++ Seat/views.py
@@ -0,0 +1,4 @@
+from django.http import HttpResponse
+
+def login(request):
+	return HttpResponse("we will see about logging you in here shortly")
