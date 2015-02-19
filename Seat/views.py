dd79de898f528fce4d909682d76fbacc0600e2d0
Sammons <bsammnz@gmail.com>
2015-02-19 13:01:18 -0800

    ben: use shortcuts s to make rendering simple html pages more simple

--- Seat/views.py
+++ Seat/views.py
@@ -1,7 +1,5 @@
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
 
 def login(request):
-	return HttpResponse(
-		loader.get_template('login.html')
-			.render(RequestContext(request)))
+	return render(request, 'login.html')
