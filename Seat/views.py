89488bfe237cba97725945b82b6071e0179aef02
Nick Pierson <nkcrpn@gmail.com>
2015-03-03 20:48:57 -0600

    Redirect from login to course page on submission

--- Seat/views.py
+++ Seat/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
 
 def login(request):
-	if (request.method == 'GET'):
-		return render(request, 'login.html')
-	if (request.method == 'POST'):
-		print request.POST
-		return render(request, 'login.html')
+    if (request.method == 'GET'):
+        return render(request, 'login.html')
+    elif (request.method == 'POST'):
+        # assume login successful for now
+        # in the future, redirect to teacher's _first_ course, from DB
+        return redirect('/courses/1')
+
+def course(request, courseNum):
+    if request.method == 'GET':
+        return render(request, 'teacher/course.html')
+    elif request.method == 'POST':
+        pass
