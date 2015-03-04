adfd20eb3029d84358fea0bdd2ac38db8b8a9d79
Nick Pierson <nkcrpn@gmail.com>
2015-03-03 21:47:43 -0600

    Extracts dashboard template

--- Seat/views.py
+++ Seat/views.py
@@ -10,8 +10,17 @@ def login(request):
         # in the future, redirect to teacher's _first_ course, from DB
         return redirect('/courses/1')
 
+# fake teacher data
+cs2050 = { 'id': '1', 'name': 'CS2050 - Algorithm Design II' }
+ece1210 = { 'id': '2', 'name': 'ECE 1210 - Digital Logic' }
+cs1000 = { 'id': '3', 'name': 'CS1000 - Introduction to Computer Science' }
+teacher = { 'name': 'Joe Teach', 'courses': [cs2050, ece1210, cs1000] }
+
 def course(request, courseNum):
     if request.method == 'GET':
-        return render(request, 'teacher/course.html')
+        context = { 'teacher': teacher,
+                    'courseNum': courseNum
+                  }
+        return render(request, 'teacher/course.html', context)
     elif request.method == 'POST':
         pass
