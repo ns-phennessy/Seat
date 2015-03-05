f4aaeee14eb92a1d593f87f495046b79e97e9af3
Patrick Hennessy <ph7vc@mail.missouri.edu>
2015-03-04 19:14:59 -0600

    Changed teacher app to 'dashboard'

---
+++ dashboard/views.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from django.shortcuts import render
+from datetime import date
+
+# fake exam data
+
+exam1 = {
+    'name': 'Exam 1',
+    'updated_at': date(2015, 2, 12),
+    'questions': [{} for i in range(15)],
+    'submissions': [{} for i in range(30)],
+    }
+exam2 = {
+    'name': 'Exam 2',
+    'updated_at': date(2015, 3, 3),
+    'questions': [{} for i in range(17)],
+    'submissions': [{} for i in range(29)],
+    }
+exams = [exam1, exam2]
+
+# fake teacher data
+
+cs2050 = {'id': '1', 'name': 'CS2050 - Algorithm Design II',
+          'exams': exams}
+ece1210 = {'id': '2', 'name': 'ECE 1210 - Digital Logic'}
+cs1000 = {'id': '3',
+          'name': 'CS1000 - Introduction to Computer Science'}
+teacher = {'name': 'Joe Teach', 'courses': [cs2050, ece1210, cs1000]}
+
+
+def course(request, courseNum):
+    if request.method == 'GET':
+        context = {'teacher': teacher, 'courseNum': courseNum,
+                   'course': cs2050}
+        return render(request, 'dashboard/course.html', context)
+    
+    elif request.method == 'POST':
+        pass
+
+
