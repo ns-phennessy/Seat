9817a9b83dce454efd3af2831096a89821e0219b
Patrick Hennessy <ph7vc@mail.missouri.edu>
2015-03-04 21:05:43 -0600

    Fixed url redirect loop

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, url
 from dashboard import views
 
 urlpatterns = patterns('',
-    url(r'^courses/(\d+)/$', views.course),
-    url(r'^exams/(\d+)/$', views.exam),
-    url(r'^exams/(\d+)/edit/$', views.exam_edit),
-    url(r'^exams/(\d+)/new/$', views.exam_new),
-    url(r'^exams/(\d+)/questions/(\d+)/$', views.exam_question)
+    url(r'^courses/(\d+)/?$', views.course),
+    url(r'^exams/(\d+)/?$', views.exam),
+    url(r'^exams/(\d+)/edit/?$', views.exam_edit),
+    url(r'^exams/(\d+)/new/?$', views.exam_new),
+    url(r'^exams/(\d+)/questions/(\d+)/?$', views.exam_question)
 )
