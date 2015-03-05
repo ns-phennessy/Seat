36904a1353cdbf5f16d83f222733a5b60133edd6
Sammons <bsammnz@gmail.com>
2015-03-04 21:14:35 -0800

    ben - merge + db being slightly brought in

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django_extensions.db.fields import UUIDField
+import uuid
 
-# Create your models here.
+class UUIDModel(models.Model):
+	guid = UUIDField(primary_key=False, unique=True, default=uuid.uuid4, editable=False)
+
+class User(UUIDModel):
+	# whatever properties all users share
+	name = models.TextField() # this is a "Suggestion" since we still have no models
+	email = models.EmailField() # sure, why not
+
+
+class Question(UUIDModel):
+	category = models.TextField()
+	text = models.TextField()
+
+class Submission(UUIDModel):
+	pass#circular reference avoidance
+
+class Exam(UUIDModel):
+	name = models.TextField()
+	updated_at = models.DateTimeField(auto_now=True)
+	questions = models.ManyToManyField(Question)
+	def submissions():
+		return Submission.objects.get(exam=id)
+
+#circular reference patch
+Submission.question = models.OneToOneField(Question)
+Submission.exam = models.ForeignKey(Exam)
+
+class Course(UUIDModel):
+	name = models.TextField()
+	exams = models.ManyToManyField(Exam)
+
+class Teacher(User):
+	courses = models.ManyToManyField(Course)
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,33 +2,68 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from datetime import date
+from dashboard.models import Teacher
+from dashboard.models import Course
+from dashboard.models import Exam
+
 
 # fake exam data
+# exam1 = {
+#     'name': 'Exam 1',
+#     'updated_at': date(2015, 2, 12),
+#     'questions': [{} for i in range(15)],
+#     'submissions': [{} for i in range(30)],
+#     }
+# exam2 = {
+#     'name': 'Exam 2',
+#     'updated_at': date(2015, 3, 3),
+#     'questions': [{} for i in range(17)],
+#     'submissions': [{} for i in range(29)],
+#     }
+# exams = [exam1, exam2]
 
-exam1 = {
-    'name': 'Exam 1',
-    'updated_at': date(2015, 2, 12),
-    'questions': [{} for i in range(15)],
-    'submissions': [{} for i in range(30)],
+# # fake teacher data
+# cs2050 = {'id': '1', 'name': 'CS2050 - Algorithm Design II', 'exams': exams}
+# teacher = {'name': 'Joe Teach'}
+
+def get_session():
+    #hacky as fuck initialize db
+    default_teacher = Teacher.objects.get_or_create(name="joe", email="guilliamsd@mizzou.edu")[0]
+    default_teacher.save()
+    default_courses = [
+        Course.objects.get_or_create(id=1,name="CS2050 - Algorithm Design II"),
+        Course.objects.get_or_create(id=2,name="ECE 1210 - Digital Logic"),
+        Course.objects.get_or_create(id=3,name="CS1000 - Introduction to Computer Science")
+    ]
+    default_exams= [
+        Exam.objects.get_or_create(id=1, name='Ex 1'),
+        Exam.objects.get_or_create(id=2, name='Ex 2'),
+    ]
+    for i in range(len(default_courses)):
+        for j in range(len(default_exams)):
+            default_exams[j][0].save()
+            default_courses[i][0].exams.add(default_exams[j][0])
+        default_courses[i][0].save()
+        default_teacher.courses.add(default_courses[i][0])
+    default_teacher.save()
+    return {
+        'user_id' : Teacher.objects.all()[0].id
     }
-exam2 = {
-    'name': 'Exam 2',
-    'updated_at': date(2015, 3, 3),
-    'questions': [{} for i in range(17)],
-    'submissions': [{} for i in range(29)],
-    }
-exams = [exam1, exam2]
-
-# fake teacher data
-cs2050 = {'id': '1', 'name': 'CS2050 - Algorithm Design II', 'exams': exams}
-ece1210 = {'id': '2', 'name': 'ECE 1210 - Digital Logic'}
-cs1000 = {'id': '3', 'name': 'CS1000 - Introduction to Computer Science'}
-teacher = {'name': 'Joe Teach', 'courses': [cs2050, ece1210, cs1000]}
-
 
 def course(request, courseNum):
     if request.method == 'GET':
-        context = {'teacher': teacher, 'courseNum': courseNum, 'course': cs2050}
+        session = get_session()
+        try:
+            teacher = Teacher.objects.get(id=session['user_id'])
+            course = Course.objects.get(id=courseNum)
+        except Course.DoesNotExist:
+            #TODO: handle error in the view
+            raise Exception("course with specified coursname failed to load")
+        context = {
+            'teacher': teacher,
+            'courseNum': courseNum,
+            'course': course.name
+            }
         return render(request, 'dashboard/course.html', context)
     elif request.method == 'POST':
         pass
