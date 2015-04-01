2806481540cf7c48f78113854e71d2c0d4ec06a4
Sammons <bsammnz@gmail.com>
2015-03-31 17:55:04 -0700

    fix verification methods before my pc dies

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,13 +5,13 @@ from seat.route_right_guards import teacher,student
 
 urlpatterns = patterns('',
 	url(r'^/?$', lambda req: redirect('courses/')),
-	url(r'^courses/?$',                 views.dashboard_index),
-    url(r'^courses/(\d+)/?$',           views.course),
-    url(r'^courses/new/?$',             views.course_new),
-    url(r'^exams/?$',                   views.exam_index),
-    url(r'^exams/(\d+)/?$',             views.exam),
-    url(r'^exams/(\d+)/edit/?$',        views.exam_edit),
-    url(r'^exams/new/?$',               views.exam),#TODO: change to own endpoint when view ready
-    url(r'^exams/(\d+)/questions/?$',   views.questions_index),
-    url(r'^questions/(\d+)/?$',         views.question)
+	url(r'^courses/?$',                 teacher(views.dashboard_index)),
+    url(r'^courses/(\d+)/?$',           teacher(views.course)),
+    url(r'^courses/new/?$',             teacher(views.course_new)),
+    url(r'^exams/?$',                   teacher(views.exam_index)),
+    url(r'^exams/(\d+)/?$',             teacher(views.exam)),
+    url(r'^exams/(\d+)/edit/?$',        teacher(views.exam_edit)),
+    url(r'^exams/new/?$',               teacher(views.exam)),#TODO: change to own endpoint when view ready
+    url(r'^exams/(\d+)/questions/?$',   teacher(views.questions_index)),
+    url(r'^questions/(\d+)/?$',         teacher(views.question))
 )
--- seat/route_right_guards.py
+++ seat/route_right_guards.py
@@ -1,8 +1,14 @@
 
-def teacher(request, func):
-	# check that the session is valid
-	return lambda request: func(request)
+def verify_teacher_and_execute_func(request, func):
+	# if invalid return something else
+	return func(request)
+
+def verify_student_and_execute_func(request, func):
+	# if invalid return something else
+	return func()
+
+def teacher(func):
+	return lambda request: verify_teacher_and_execute_func(request, func)
 	
-def student(request, func):
-	# check that the session is valid
-	return lambda request: func(request)
+def student(func):
+	return lambda request: verify_student_and_execute_func(request)
