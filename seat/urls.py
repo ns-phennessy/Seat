a42c5935921cbe71da0078afe89af1e8b5e9e87e
Sammons <bsammnz@gmail.com>
2015-04-01 00:27:59 -0500

    still reorganizing, some of the stupid files I just made died, also fixed some mistakes - when it complains about a WSGI object that's a bug where request[] is used instead of request.session[]

--- seat/urls.py
+++ seat/urls.py
@@ -4,16 +4,17 @@ from django.shortcuts import redirect
 
 # obliterates user session
 def logout(request):
+	if request.session['user']:
+		print "Logging user out", request.session['user'] 
 	for key in request.session.keys():
 		del request.session[key]
-	print "Loging user out"
 	request.session.flush() # force this through the db
 	return redirect('/login/')
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/', include('login.urls')),
-    url(r'^dashboard/', include('dashboard.urls')),
     url(r'^$', lambda request: redirect('/login/')),
-    url(r'^logout/$', logout)
+    url(r'^logout/$', logout),
+    url(r'^login/', include('login.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^dashboard/', include('dashboard.urls')),
 )
