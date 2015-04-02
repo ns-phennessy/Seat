a7fe306bdbc0bab04b88c9551b03e20e37195d7d
Nick Pierson <nkcrpn@gmail.com>
2015-04-02 18:51:41 -0500

    Create consistency among DELETE requests

--- static/js/exam.js
+++ static/js/exam.js
@@ -1,22 +1,19 @@
 $('#addquestion').on('click', function(){
     $(this).hide();
-  
+
     $('#newquestionform').appendTo($(this).parent());
     $('#newquestionform').slideDown(100)
 });
 
 $('div[data-role=question] div[data-role=delete]').on('click', function() {
-    var question = $(this).closest('div[data-role=question]');
-    var questionId = question.data('id');
-
     $.ajax({
-        url: '/dashboard/questions/' + questionId,
+        url: $(this).data('action');
         type: 'DELETE',
         beforeSend: function(xhr) {
           xhr.setRequestHeader('X-CSRFToken', $.cookie('csrftoken'));
         },
         success: function() {
-          question.remove();
+          location.reload();
         }
     });
 });
