9aac25ef519678221ced133ca24ba71a16bac898
Patrick Henenssy <ph7vc@mail.missouri.edu>
2015-04-08 12:53:35 -0500

    Updated prod settings name so DJango can recognize it. Updated wsgi.py to use production settings when being run in a WSGI environmentw

--- seat/wsgi.py
+++ seat/wsgi.py
@@ -15,7 +15,7 @@ basePath = '/var/www/capstone'
 sys.path.append(basePath)
 sys.path.append(basePath + '/seat')
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "seat.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "seat.settings-prod"
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
