2ac4d04a10d032293629cc8513f7154d42e37dbb
Nick Pierson <nkcrpn@gmail.com>
2015-04-15 11:29:29 -0500

    Add modal animations. Fix input name

--- static/js/new-exam.js
+++ static/js/new-exam.js
@@ -1,3 +1,22 @@
+$('#newExamModal.ui.modal').modal(
+	{
+		transition:'fly down',
+		duration:500,
+		closable:false,
+		selector:{
+			approve:'.actions .save'
+		},
+		onShow:function(){
+			$(this).find('.ui.form').removeClass('loading');
+      $(this).find('input[name=exam-name]').val('');
+      $(this).find('input[name=course]').val('');
+		},
+		onApprove: function(){
+			return false;
+		}
+	}
+);
+
 $('#newExamBtn').on('click', function() {
   $('#newExamModal.ui.modal').modal('show');
 });
