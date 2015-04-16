75e93d617c748b840d574182829f7356df5194d6
Sammons <bsammnz@gmail.com>
2015-04-16 00:49:50 -0500

    make tokens unique

--- seat/models/token.py
+++ seat/models/token.py
@@ -7,7 +7,7 @@ def random_token():
 
 class Token(models.Model):
     """key to taking an exam"""
-    token = models.TextField(default=random_token)
+    token = models.TextField(default=random_token, unique=True)
     exam = models.ForeignKey(Exam)
     open = models.BooleanField(default=False)
     released = models.BooleanField(default=False)
