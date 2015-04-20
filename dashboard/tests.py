1ce10fe21b09a3329b230127ad8df7ca1c0c4036
gsatherlie <poochay@gmail.com>
2015-04-19 19:45:13 -0500

    Rebased and corrected tests to reflect updates

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -8,83 +8,51 @@ from django.http import *
 from django.test.client import RequestFactory
 from django.test import Client
 from seat.models.teacher import Teacher
-from seat.models.exam import Question, Submission, Exam
+from seat.models.exam import Question, Exam
 from seat.models.course import Course
 from django.utils.importlib import import_module
 
 # Create your tests here.
 class DashboardUnitTests(TestCase):
     def setUp(self):
-   #     self.client = Client()
+        self.client = Client()
         Teacher.objects.create(name="fred", email="fred@fred.com")
-     #   s = self.client.session
-     #   s['user_id'] = 1
-     #   s.save()
-        # Every test needs access to the request factory.
-        self.factory = RequestFactory()
+
         settings.SESSION_ENGINE = 'django.contrib.sessions.backends.file'
         engine = import_module(settings.SESSION_ENGINE)
         store = engine.SessionStore()
         store.save()
         self.session = store
         self.client.cookies[settings.SESSION_COOKIE_NAME] = store.session_key
-
-    def test_tickles(self):
-        session = self.session
-        session['user_id'] = 1;
-        session.method = 'GET'
-        session.save()
-
-        answer = dashboard_index(session)
-
-        self.assertEquals(answer.status_code, 302)
-
 
     def test_index_rejects_anything_but_get_requests(self):
         request = HttpRequest
         request.method = 'POST'
         answer = dashboard_index(request)
-        self.assertEquals('/invalidrequest/', answer.status_code)
+
+        self.assertContains(answer, "invalid request")
 
     def test_index_enforces_teachers_only(self):
-        request = self.client.get('/')
-        response = dashboard_index(request)
-        self.assertEquals(302, response.status_code)
+        s = self.client.session
+        s['user_id'] = '2'
+        s.save()
+        response = self.client.get('/dashboard/')
 
-    def test_incorrect_params_throws_error(self):
-        #Needs session
-        session = self.session
-        session['user_id'] = 2;
-        session.method = 'GET'
-        session.save()
-
-        try:
-            response = dashboard_index(session)
-            #teacher = teacherApplication.get_teacher_by_id('2')
-        except Exception, error:
-            assert(Exception('failed to get_teacher_by_id with id:','2'), error)
-        assert(True)
+        self.assertContains(response, 'not a teacher')
 
     def test_correct_params_redirect_to_teacher_page(self):
-        #Needs session
-        #request = HttpRequest
-        #request.method = 'GET'
-        #request.session['user_id':1]
-        request = self.client.get('/')
-        response = dashboard_index(request)
-        #print request.session['user_id']
-        #self.assertEquals('/dashboard/courses/', redirect( teacherApplication.landing_page_url(response)))
-        assert(True)
+        s = self.client.session
+        s['user_id'] = '1'
+        s.save()
+        response = self.client.get('/dashboard/')
+
+        self.assertEquals('http://testserver/dashboard/courses/', response.url)
 
 class CoursesUnitTests(TestCase):
     def setUp(self):
-   #     self.client = Client()
+        self.client = Client()
         Teacher.objects.create(name="fred", email="fred@fred.com")
-     #   s = self.client.session
-     #   s['user_id'] = 1
-     #   s.save()
-        # Every test needs access to the request factory.
-        self.factory = RequestFactory()
+
         settings.SESSION_ENGINE = 'django.contrib.sessions.backends.file'
         engine = import_module(settings.SESSION_ENGINE)
         store = engine.SessionStore()
@@ -92,15 +60,18 @@ class CoursesUnitTests(TestCase):
         self.session = store
         self.client.cookies[settings.SESSION_COOKIE_NAME] = store.session_key
 
-    def test_index_rejects_anything_but_get_requests(self):
+    def test_course_rejects_anything_but_get_requests(self):
         request = HttpRequest
         request.method = 'POST'
         answer = courses(request, 10)
-        self.assertEquals('/invalidrequest/', answer.status_code)
 
-    def test_courses_displays_first_course_if_no_id(self):
-        request = self.client.get('/courses/')
-        response = courses(request, 0)
+        self.assertContains(answer, "invalid method")
 
-        self.assertContains(response, 'stuff')
+    def test_courses_displays_no_courses_if_none_are_made(self):
+        s = self.client.session
+        s['user_id'] = '1'
+        s.save()
+        response = self.client.get('/dashboard/courses/')
+
+        self.assertContains(response, 'without a course')
 
