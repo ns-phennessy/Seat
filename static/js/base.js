270b247df31644186cd5885c0f831109404b1bbc
Nick Pierson <nkcrpn@gmail.com>
2015-04-21 19:42:07 -0500

    Refresh page if loaded from cache for all dashboard pages

--- static/js/base.js
+++ static/js/base.js
@@ -5,3 +5,12 @@ $('[data-content]').popup({
 $('.ui.menu .item').tab();
 $('.ui.dropdown:not([data-role="questionType"])').dropdown({});
 $('.ui.checkbox').checkbox();
+
+$(document).ready(function(e) {
+  var refresh = $('#refresh');
+  if ( refresh.val() === 'yes' ) {
+    location.reload(true);
+  } else {
+    refresh.val('yes');
+  }
+});
