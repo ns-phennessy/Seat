cd5f95d01202d075219584d6fa260d7f76baf4cf
Patrick Hennessy <ph7vc@mail.missouri.edu>
2015-04-23 22:49:33 -0500

    Added Ben's JS for token CRUD operations

--- static/js/course.js
+++ static/js/course.js
@@ -1,22 +1,80 @@
 $('tr[data-role=exam] div[data-role=delete]').on('click', function(e) {
-  var optionDelete = this;
+	var optionDelete = this;
 
-  data = { exam_id: $(this).data('id') };
+	data = { exam_id: $(this).data('id') };
 
-  $.ajax({
-    type: 'POST',
-    url:  '/api/exam/',
-    data: data,
-    beforeSend: function(xhr) {
-      xhr.setRequestHeader('X-CSRFToken', $.cookie('csrftoken'));
-      xhr.setRequestHeader('X-METHODOVERRIDE', 'DELETE');
-    },
-    success: function() {
-      location.reload()
-    }
-  });
+	$.ajax({
+		type: 'POST',
+		url:  '/api/exam/',
+		data: data,
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader('X-CSRFToken', $.cookie('csrftoken'));
+			xhr.setRequestHeader('X-METHODOVERRIDE', 'DELETE');
+		},
+		success: function() {
+			location.reload()
+		}
+	});
 });
 
-$('.ui.accordion')
-  .accordion()
-;
+// Token stuff
+
+// Delete
+var token = function(){
+
+	this.create = function(){
+		$.ajax({
+			method: "POST",
+			url: '/api/token/',
+			data: {
+				'exam_id' : 2
+			},
+			headers: {
+				'X-CSRFToken' : $.cookie('csrftoken')
+			}
+		}).success(function(data, succcess) {console.log(data.token) })
+		.fail(function() {console.log(arguments) })
+		.always(function() {console.log(arguments) })
+		
+	};
+
+	this.delete = function(){
+		$.ajax({
+			method: "POST",
+			url: '/api/token/',
+			data: {
+				'token_id' : 15
+			},
+			headers: {
+				'X-CSRFToken' : $.cookie('csrftoken'),
+				'X-METHODOVERRIDE' : 'DELETE'
+			}
+		}).success(function(data, succcess) {console.log(data.token) })
+		.fail(function() {console.log(arguments) })
+		.always(function() {console.log(arguments) })
+	};
+
+	this.update = function(){
+		$.ajax({
+			method: "POST",
+			url: '/api/token/',
+			data: {
+				'token' : JSON.stringify({
+					'token_id' : 1,
+					'open' : true,
+					'released' : false
+				})
+			},
+			headers: {
+				'X-CSRFToken' : $.cookie('csrftoken'),
+				'X-METHODOVERRIDE' : 'PUT'
+			}
+		},function() {
+			console.log(arguments)
+		})
+	};
+
+
+}
+
+
