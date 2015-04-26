2a624702ac1a3a6798ed3e5afd23bbe063490d4a
Sammons <bsammnz@gmail.com>
2015-04-25 22:47:21 -0500

    flesh out GET methods for questions in case we need them

--- api/resources/question_methods.py
+++ api/resources/question_methods.py
@@ -1,4 +1,5 @@
 from seat.applications.QuestionApplication import QuestionApplication
+from seat.models.exam import Question
 from django.http import JsonResponse
 from api.helpers import endpoint_checks
 import json
@@ -27,13 +28,15 @@ def upsert_question_logic(teacher, request):
     try:
         question = json.loads(request.POST.get('question'))
         exam_id = request.POST['exam_id']
+        if not endpoint_checks.id_is_valid(exam_id):
+            return upsert_question_failure_json_model("Invalid exam id")
         modified_question, message = questionApplication.upsert_question(teacher, exam_id, question)
         if modified_question == False:
             return upsert_question_failure_json_model(message)
         return upsert_question_success_json_model(modified_question.id)
     except Exception as error:
         logger.warn("problem upserting question! :"+str(error))
-        return upsert_question_failure_json_model('failed to upsert the question, sorry. This is probably a db error.')
+        return upsert_question_failure_json_model('Failed to upsert the question, sorry. This is probably a database error.')
 
 def upsert_question(request):
     return endpoint_checks.standard_teacher_endpoint(
@@ -61,13 +64,15 @@ def delete_question_failure_json_model(message):
 def delete_question_logic(teacher, request):
     try:
         question_id = request.DELETE['question_id']
+        if not endpoint_checks.id_is_valid(question_id):
+            return delete_question_failure_json_model("Invalid question id")
         success, message = questionApplication.delete_question(teacher, question_id)
         if not success:
             return delete_question_failure_json_model(message)
         return delete_question_success_json_model()
     except Exception, error:
         logger.warn("problem deleting question! :"+str(error))
-        return delete_question_failure_json_model('failed to delete the question, sorry. This is probably a db error.')
+        return delete_question_failure_json_model('Failed to delete the question, sorry. This is probably a database error.')
 
 def delete_question(request):
     return endpoint_checks.standard_teacher_endpoint(
@@ -95,16 +100,23 @@ def get_question_failure_json_model(message):
 
 def get_question_logic(teacher, request):
     try:
-        #TODO: test that teacher owns resource
-        raise Exception("UNSUPPORTED")
+        question_id = request.GET['question_id']
+        if not endpoint_checks.id_is_valid(question_id):
+            return get_question_failure_json_model("Invalid question id")
+
+        question = Question.objects.filter(id=question_id, exam__course__teacher=teacher)
+        if not question.exists():
+            return get_question_failure_json_model("Question not found")
+
+        return get_question_success_json_model(question)
     except Exception, error:
         logger.warn("problem getting question! :"+str(error))
-        return get_question_failure_json_model('failed to get the question, sorry. This is probably a db error.')
+        return get_question_failure_json_model('Failed to get the question, sorry. This is probably a database error.')
 
 def get_question(request):
     return endpoint_checks.standard_teacher_endpoint(
         "get_question",
-        ['exam_id', 'question'],
+        ['question_id'],
         'POST',
         request,
         get_question_logic
